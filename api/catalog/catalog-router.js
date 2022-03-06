e01615f7541104845042bd6396e155e95b0cd6d0
Nelson Maldonado <play2nino@gmail.com>
2022-03-05 22:08:20 -0800

    wip

--- api/catalog/catalog-router.js
+++ api/catalog/catalog-router.js
@@ -1,8 +1,8 @@
 const router = require("express").Router()
 
-router.get("*", (req, res) => {
-  res.send("Error 404")
-})
+// router.get("*", (req, res) => {
+//   res.send("Error 404")
+// })
 
 router.get("/", (req, res) => {
   res.send("getting all users")

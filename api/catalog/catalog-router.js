2e6792296cd9df0238195ff610883ab873c723ae
Nelson Maldonado <play2nino@gmail.com>
2022-03-05 19:28:01 -0800

    endpoint test

---
+++ api/catalog/catalog-router.js
@@ -0,0 +1,17 @@
+const router = require("express").Router()
+
+router.get("/", (req, res) => {
+  res.send("getting all users")
+})
+// router.get("/", async (req, res) => {
+//     res.json(await getAllUsers())
+//   })
+
+router.post("/register", (req, res) => {
+  res.send("user registered")
+})
+
+//   server.post("/api/users", async (req, res) => {
+//     res.status(201).json(await insertUser(req.body))
+//   })
+module.exports = router

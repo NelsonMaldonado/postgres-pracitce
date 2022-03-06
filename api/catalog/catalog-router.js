f50c8966c4b524573ff2a7bae4f0a4d06308c6d4
Nelson Maldonado <play2nino@gmail.com>
2022-03-05 19:34:54 -0800

    get all users endpoint

--- api/catalog/catalog-router.js
+++ api/catalog/catalog-router.js
@@ -1,17 +1,40 @@
 const router = require("express").Router()
+const db = require("./data/db-config")
 
-router.get("/", (req, res) => {
-  res.send("getting all users")
-})
-// router.get("/", async (req, res) => {
-//     res.json(await getAllUsers())
-//   })
-
-router.post("/register", (req, res) => {
-  res.send("user registered")
+function getAllsteps() {
+  return db("steps")
+}
+router.get("/api/steps", async (req, res) => {
+  res.json(await getAllsteps())
 })
 
-//   server.post("/api/users", async (req, res) => {
-//     res.status(201).json(await insertUser(req.body))
-//   })
+async function insertUser(user) {
+  // WITH POSTGRES WE CAN PASS A "RETURNING ARRAY" AS 2ND ARGUMENT TO knex.insert/update
+  // AND OBTAIN WHATEVER COLUMNS WE NEED FROM THE NEWLY CREATED/UPDATED RECORD
+  const [newUserObject] = await db("steps").insert(user, [
+    "user_id",
+    "username",
+    "password",
+  ])
+  return newUserObject // { user_id: 7, username: 'foo', password: 'xxxxxxx' }
+}
+
+router.post("/api/steps", async (req, res) => {
+  res.status(201).json(await insertUser(req.body))
+})
+
+// router.get("/", (req, res) => {
+//   res.send("getting all users")
+// })
+router.get("/", async (req, res) => {
+  res.json(await getAllsteps())
+})
+
+// router.post("/register", (req, res) => {
+//   res.send("user registered")
+// })
+
+router.post("/api/users", async (req, res) => {
+  res.status(201).json(await insertUser(req.body))
+})
 module.exports = router
